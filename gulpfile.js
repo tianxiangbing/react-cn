4dbc3ea63fb9029c3b531f3a99959bdc3573097f
tianxiangbing <55342775@qq.com>
2016-08-08 21:01:07 +0800

    init

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp');
+var livereload = require('gulp-livereload');
+var fileinclude = require('gulp-file-include');
+var sass = require('gulp-ruby-sass');
+//var scss =require('gulp-sass');
+
+gulp.task('html', function() {
+	gulp.src('src/**/*.html')
+	.pipe(fileinclude({
+		prefix:"@@",
+		basepath:'src/component/'
+	}))
+	.pipe(gulp.dest('./build'))
+	.pipe(livereload())
+	;
+});
+gulp.task('css',function(){
+	sass('src/**/css.scss')
+	.pipe(gulp.dest('./build/'))
+    .on('error', sass.logError)
+	.pipe(livereload());
+});
+/*
+gulp.task('sass',function(){
+	gulp.src("src//.scss").pipe(scss())
+	.pipe(gulp.dest('./build'))
+	.pipe(livereload());
+});
+*/
+//watch
+gulp.task('watch', function() {
+	livereload.listen();
+	var server = livereload();
+	gulp.watch(['src/**/*.html','src/**/*.htm'], ['html']);
+	gulp.watch('src/**/*.scss',['css'])
+});
+
+gulp.task('default',['html','css'])
